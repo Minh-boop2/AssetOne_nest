a5f9382158e8979eefd9d8744219208f845ee959
Minh-boop2 <tranvinh18asd@gmail.com>
2026-04-23 09:33:38 +0700

    add run.bat for database

--- run.bat
+++ run.bat
@@ -1,4 +1,18 @@
 @echo off
+REM Kiểm tra môi trường ảo đang active
+if not exist "venv\Scripts\activate" (
+    echo "Chua co venv. Tao moi..."
+    python -m venv venv
+)
 
+REM Kích hoạt venv
+call venv\Scripts\activate
+
+REM Cài các thư viện nếu chưa có
+pip install --upgrade pip
+pip install -r requirements.txt
+
+REM Chạy app
 python app.py
+
 pause
